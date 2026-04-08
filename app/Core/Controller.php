0a77bb06e010fb842c6bf3487d3e2c13c0b690aa
FindoRZH_ <Findorizhanta05@gmail.com>
2026-04-08 13:07:37 +0700

    routing

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -1,19 +1,18 @@
 <?php
 
 class Controller {
-    // Fungsi untuk memanggil file View
     public function view($view, $data = []) {
         
-        // Cek koneksi database setiap kali view dipanggil
         $db = new Database();
         $data['db_status'] = $db->isConnected;
 
-        require_once '../views/' . $view . '.php';
+        extract($data);
+
+        require_once __DIR__ . '/../../views/' . $view . '.php';
     }
 
-    // Fungsi untuk memanggil file Model
     public function model($model) {
-        require_once __DIR__ . '../app/Models/' . $model . '.php';
+        require_once __DIR__ . '/../../app/Models/' . $model . '.php';
         return new $model;
     }
 }
