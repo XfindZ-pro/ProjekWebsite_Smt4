6ad9d2e9e42042d1dd45f537453aaa2a70a48427
FindoRZH_ <Findorizhanta05@gmail.com>
2026-04-08 12:58:13 +0700

    routing baru

--- app/Core/App.php
+++ app/Core/App.php
@@ -1,7 +1,6 @@
 <?php
 
 class App {
-    // Properti default jika URL kosong
     protected $controller = 'Home';
     protected $method = 'index';
     protected $params = [];
@@ -9,47 +8,38 @@ class App {
     public function __construct() {
         $url = $this->parseURL();
         
-      // 1. Setup Controller
-      if(isset($url[0])) {
-        // Cek apakah file controller-nya ada
-        if(file_exists('../app/Controllers/' . $url[0] . '.php')) {
-            $this->controller = $url[0];
-            unset($url[0]);
-        } else {
-            // JIKA CONTROLLER TIDAK ADA -> Arahkan ke NotFound
-            $this->controller = 'NotFound';
-            unset($url[0]);
+        if(isset($url[0])) {
+            if(file_exists(__DIR__ . '/../Controllers/' . $url[0] . '.php')) {
+                $this->controller = $url[0];
+                unset($url[0]);
+            } else {
+                $this->controller = 'NotFound';
+                unset($url[0]);
+            }
         }
-    }
-    
-    // Panggil dan instansiasi controller-nya
-    require_once '../app/Controllers/' . $this->controller . '.php';
-    $this->controller = new $this->controller;
+        
+        require_once __DIR__ . '/../Controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
 
-        // 2. Setup Method
         if(isset($url[1])) {
-            // Cek apakah method ada di dalam controller tersebut
             if(method_exists($this->controller, $url[1])) {
                 $this->method = $url[1];
                 unset($url[1]);
             }
         }
 
-        // 3. Setup Params
         if(!empty($url)) {
-            $this->params = array_values($url); // Mengambil sisa array sebagai parameter
+            $this->params = array_values($url);
         }
 
-        // 4. Jalankan controller & method, serta kirim parameter jika ada
         call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
-    // Fungsi untuk mengambil dan membersihkan URL
     public function parseURL() {
         if(isset($_GET['url'])) {
-            $url = rtrim($_GET['url'], '/'); // Hapus garis miring di akhir
-            $url = filter_var($url, FILTER_SANITIZE_URL); // Bersihkan dari karakter aneh
-            $url = explode('/', $url); // Pecah URL berdasarkan garis miring '/'
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
             return $url;
         }
         return [];
